bf194152a4054fcdc6f1673ab8d7a466ddd9b35c
LucasManuelRodriguez <rodriguez.m.lucas@gmail.com>
2021-08-20 05:16:10 -0300

    nuevo

---
+++ main.js
@@ -0,0 +1,27 @@
+document.addEventListener("click", function(event){
+    let nombreBoton = event.target.dataset.boton; 
+    let elemento = event.target;
+
+    if(nombreBoton){  
+    let audioBoton = document.getElementById(nombreBoton); 
+    audioBoton.currentTime=0;
+    audioBoton.play();
+    elemento.classList.add('presionado'); 
+    // setTimeout(function(){
+    //     elemento.classList.add('noPresionado'); 
+    // }, 800);
+
+
+    
+    
+}   
+
+
+
+
+        
+});
+
+
+
+
